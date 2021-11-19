66c8b2ad279d7be4e0bdd055b1efc047adcb14a8
unknown <layard3@gmail.com>
2021-11-19 19:57:09 +0800

    create models and database

--- store/models.py
+++ store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
 class Category(models.Model):
@@ -29,4 +32,21 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     # not necessary to put image here, because we can use another table like album table
     image = models.ImageField(upload_to='images/')
+    # urls last unique place that we can utilize it
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    # products that are active to buy
+    is_active = models.BooleanField(default=True)
+    # auto_now_add is going to happen only once
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        verbose_name_plural = 'Products'
+        # '-created' will show the latest created product first
+        # 'created' will reverse
+        ordering = ('-created',)
+
+    def __str__(self) -> str:
+        return self.title

32bf6b74591b4b4291c85ed5321f3885410a483d
unknown <layard3@gmail.com>
2021-11-21 07:38:14 +0800

    updated views -product

--- store/views.py
+++ store/views.py
@@ -17,9 +17,9 @@ def all_products(request):
 # urls -> views -> models
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'products': product})
+    return render(request, 'store/products/detail.html', {'product': product})
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', {'category': category, 'products': products})
+    return render(request, 'store/products/category.html', {'category': category, 'product': product})
